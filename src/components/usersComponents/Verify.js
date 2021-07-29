4f560e3d1a6843d58b3be569a58598f34c35c44d
Yousef-laban <yousef.labn@fgmail.com>
2021-07-29 20:35:00 +0300

    addToastMessage

--- src/components/usersComponents/Verify.js
+++ src/components/usersComponents/Verify.js
@@ -1,29 +1,42 @@
 import { useDispatch } from "react-redux";
 
-
-
-// import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { useSelector } from "react-redux";
 import { verify } from "../../store/actions/authActions";
-import "../../App.css"
+import "../../App.css";
 
 const Verify = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
 
+  const handelVerify = () => {
+    dispatch(verify(user));
+    toast("Check Your Email To Verify", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   return (
     <>
-      <h2 className="b8"> !!  Please Verify Your Email  !! </h2>
+      <h2 className="b8"> !! Please Verify Your Email !! </h2>
 
       <button
         style={{ margin: "10px" }}
         type="button"
         class="b9"
-        onClick={() => dispatch(verify(user))}
+        onClick={handelVerify}
       >
         Verify
       </button>
+      <ToastContainer />
     </>
   );
 };
